b2a0a360d18ea6616ada284c1d04f46ee937fa54
maribogl <maribogl@yandex.ru>
2022-10-20 15:58:14 +0300

    tasks

---
+++ task41/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите числа через запятую: ");
+string[] numbers = Console.ReadLine().Split(new char[] { ',' }); // Сразу разбиваем строку на элементы, разделитель - ','
+int[] arrayNumbers = StringToArray(numbers);
+int countPositiveNum = CountPositiveNum(arrayNumbers);
+Console.WriteLine($"Количество введеных чисел больше нуля -> {countPositiveNum}");
+
+int CountPositiveNum(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count += 1;
+    }
+    return count;
+}
+
+int[] StringToArray(string[] num)
+{
+    int[] array = new int[num.Length];
+    for (int i = 0; i < num.Length; i++)
+    {
+        array[i] = Convert.ToInt32(num[i]);
+    }
+    return array;
+}
---
+++ task43/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите значение b1: ");
+double numBFirst = Convert.ToDouble (Console.ReadLine());
+
+Console.WriteLine("Введите значение k1: ");
+double numKFirst = Convert.ToDouble (Console.ReadLine());
+
+Console.WriteLine("Введите значение b2: ");
+double numBSecond = Convert.ToDouble (Console.ReadLine());
+
+Console.WriteLine("Введите значение k2: ");
+double numKSecond = Convert.ToDouble (Console.ReadLine());
+
+double [] res = SearchPointCoordinat(numBFirst,numKFirst, numBSecond,numKSecond);
+Console.WriteLine($"Прямые пересекутся в точке с координатами ({res[0]}, {res[1]})");
+
+
+double [] SearchPointCoordinat (double b1,double k1, double b2, double k2)
+{
+    double x= (b2-b1)/ (k1-k2);
+    double y =  k1*x+b1;
+ double [] pointCoord = {Math.Round (x,2),Math.Round (y,2)};
+ return pointCoord;
+}
